7b7a9da6b5d0831479173290aa9fd0aac86242c7
Xica Felix <xicasfelix@gmail.com>
2021-11-23 13:05:11 -0500

    commit tree 8c4294118de65a4f7b37c22e0bc79cdd273ba019

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride >= 2500) {
+    return 'No can do.';
+  } else if (ride > 2000){
+    return 'I will gladly take your thirty bucks.';  
+  } else if(ride <= 400){
+    return 'This one is on me!';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let answer;
+  city === 'NYC' ? ( answer ='Ok, sounds good.'): (answer= 'No go.');
+  return answer;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let answer;
+  switch (true) {
+    case tip==="generous":
+      answer= "Thank you so much."
+      break;
+    case tip==="not as generous":
+      answer="Thank you." 
+      break;
+    default:
+      answer="Bye." 
+      break;
+  }
+  return answer;
 }
+
